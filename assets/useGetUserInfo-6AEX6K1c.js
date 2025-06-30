68b14ea53b47b5be7b739b56f65225f300b44cf0
hholb <hholb@users.noreply.github.com>
2025-06-30 15:50:44 +0000

    pull cache population out of 'useQuery{(select: ()=>{}}' callbacks (#393) Garden-AI/garden-frontend@1a613151928637009201530d37a86b75acd5657a

---
+++ assets/useGetUserInfo-6AEX6K1c.js
@@ -0,0 +1,1 @@
+import{i as e}from"./card-Bfx9cp5E.js";import{u as s}from"./useQuery-5VXMb_iS.js";import{u as t}from"./index-Db6X0dgS.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
