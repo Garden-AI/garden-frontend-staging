e3a080f19422c9c167358fc6bf1351eb377fc3ff
hholb <hholb@users.noreply.github.com>
2025-07-02 15:34:23 +0000

    update hayden's super user id (#396) Garden-AI/garden-frontend@3ac4e3ff6f9448955c6f50e8aba0ac867c011f87

---
+++ assets/useGetModalFunction-D5DdLFgk.js
@@ -0,0 +1,1 @@
+import{i as t}from"./card-Bs94xfu6.js";import{u as a}from"./useQuery-Bvk_3ITv.js";const u=n=>a({queryKey:["modalFunctions",Number(n)],queryFn:async()=>{const o=(await t.get(`/modal-functions/${n}`)).data,e=await t.get(`/modal-apps/${o.modal_app_id}`);return{...o,owner_identity_id:e.data.owner_identity_id}}});export{u};
