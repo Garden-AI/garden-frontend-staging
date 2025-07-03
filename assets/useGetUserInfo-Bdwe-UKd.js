31c755cb3c1b421542a3de3af993dd6af48866e5
nhouston125 <nhouston125@users.noreply.github.com>
2025-07-03 18:30:43 +0000

    Redeployment Flow Update
    * UX Update: Redeploying a Modal App
    
    * pull cache population out of 'useQuery{(select: ()=>{}}' callbacks (#393)
    
    * Updated conditional rendering + dialog box + user profile tabs
    
    * update hayden's super user id (#396)
    
    * Updated tab logic for type issue
    
    ---------
    
    Co-authored-by: Hayden Holbrook <111379706+hholb@users.noreply.github.com> Garden-AI/garden-frontend@ed33bbc334ef6195d560c5bfa5acbc504e1b13f0

--- assets/useGetUserInfo-Bdwe-UKd.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./card-Bs94xfu6.js";import{u as s}from"./useQuery-Bvk_3ITv.js";import{u as t}from"./index-C6Yj70zX.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
