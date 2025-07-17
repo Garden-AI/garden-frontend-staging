f1b62f1ed18852861de749a1c8acab262eed10b0
hholb <hholb@users.noreply.github.com>
2025-07-17 16:36:24 +0000

    add hard-coded mlip-garden (#401) Garden-AI/garden-frontend@9bc6b085b8d138cc8f160934bf88d29e5acf3e42

---
+++ assets/useGetUserInfo-BwVU2Dq7.js
@@ -0,0 +1,1 @@
+import{i as e}from"./card-Weqp0p_O.js";import{u as s}from"./useQuery-DMwZsJY6.js";import{b as t}from"./index-CNHTD24i.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
