b9176943fe30547f183de03fdedaad60416c91fa
nhouston125 <nhouston125@users.noreply.github.com>
2025-07-17 14:04:28 +0000

    Function Selection Table Component Update (#402) Garden-AI/garden-frontend@f679656be52c27b6ad47df521441760ce7d817a6

---
+++ assets/useGetUserInfo-BUtlvOBy.js
@@ -0,0 +1,1 @@
+import{i as e}from"./card-BUNFGEVE.js";import{u as s}from"./useQuery-Czz-a9KE.js";import{b as t}from"./index-BPkOsgk3.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
