c2390e26b3a2a1ee5e0a334bb2def233bb29e6fb
WillEngler <WillEngler@users.noreply.github.com>
2025-08-06 22:48:18 +0000

    Update copy and layout for "above the fold" MLIP content (#412) Garden-AI/garden-frontend@9946c2cf28ffab64a4b41d7abc7e80f8d42957fa

---
+++ assets/useGetUserInfo-CjXolyr1.js
@@ -0,0 +1,1 @@
+import{i as e}from"./axios-olCs1I7A.js";import{u as s}from"./useQuery-BFcR8ybc.js";import{b as t}from"./index-HUiQ-16W.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
