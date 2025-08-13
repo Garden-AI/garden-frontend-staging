a9435a45024f61af5163713c022152c0755ef97e
WillEngler <WillEngler@users.noreply.github.com>
2025-08-13 14:55:52 +0000

    update with actual 7net time (#423) Garden-AI/garden-frontend@830cd83c5864389369c4d4296bf76293436f7218

---
+++ assets/useGetUserInfo-CX6ob-GE.js
@@ -0,0 +1,1 @@
+import{i as e}from"./card-CD7QIy1J.js";import{u as s}from"./useQuery-NovrIOqy.js";import{b as t}from"./index-BeJDqYR9.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
--- assets/useGetUserInfo-_J1SQHDI.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./card-DXpheptC.js";import{u as s}from"./useQuery-DWtWkpFL.js";import{b as t}from"./index-BAMOFai4.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
