aa61719a4525ccdd8c27b8ddec466c004f1babdb
WillEngler <WillEngler@users.noreply.github.com>
2025-09-04 22:47:53 +0000

    change to new shared support email (#438) Garden-AI/garden-frontend@4edd95ef9370c273ad07ef1994ff84f143f798d6

---
+++ assets/useGetUserInfo-C2cWZ7Ab.js
@@ -0,0 +1,1 @@
+import{i as e}from"./card-ef-iysdq.js";import{u as s}from"./useQuery-BtqBVhlC.js";import{b as t}from"./index-Dmf1vp6L.js";const o=async()=>{try{return(await e.get("/users")).data}catch{throw new Error("Error fetching user info")}},c=()=>{const r=t();return s({queryKey:["user","me"],queryFn:()=>o(),enabled:r.isAuthenticated})};export{c as u};
