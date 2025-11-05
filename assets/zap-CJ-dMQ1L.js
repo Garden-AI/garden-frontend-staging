a791d4044d74a699678d8799d6ae42ef25634197
hholb <hholb@users.noreply.github.com>
2025-11-05 22:36:07 +0000

    remove user profile components, update nav to UGMI (#470) Garden-AI/garden-frontend@2fba0b9dc50153aa812e5d82477c352a34bcf664

--- assets/zap-CJ-dMQ1L.js
+++ assets/zap-CJ-dMQ1L.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CkkkesRB.js";/**
+import{c as a}from"./index-Bty07G6q.js";/**
  * @license lucide-react v0.394.0 - ISC
  *
  * This source code is licensed under the ISC license.
