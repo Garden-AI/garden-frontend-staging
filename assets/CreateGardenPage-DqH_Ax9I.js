0138e16234d1a1756d6e68706d3c9d3d5cf07439
hholb <hholb@users.noreply.github.com>
2025-12-19 20:48:25 +0000

    Revamp benchmark page for new matbench implementation (#476)
    * remove unneeded components and api calls
    
    * update benchmarks data for new metrics, clean up presentation
    
    * add bar and parallel coodinates charts
    
    * add benchmark link to navbar Garden-AI/garden-frontend@2cccccd4456ba339f43d5d2c46ea42c4bfe8717b

--- assets/CreateGardenPage-DqH_Ax9I.js
+++ assets/CreateGardenPage-DqH_Ax9I.js
@@ -1,4 +1,4 @@
-import{c as v,u as m,j as e,a as d,b as i,d as b,e as x,f as p,g as u,B as h,h as C,i as w,r as F,O as l,k as P}from"./index-AJzcSfHz.js";import{u as S,G as k}from"./GlobusGroupError-DjQDnlhw.js";import G from"./ModalAppUploadPage-DRPJskdI.js";/**
+import{c as v,u as m,j as e,a as d,b as i,d as b,e as x,f as p,g as u,B as h,h as C,i as w,r as F,O as l,k as P}from"./index-cAh4-QJL.js";import{u as S,G as k}from"./GlobusGroupError-BuAMvjye.js";import G from"./ModalAppUploadPage-Uw-GWuOI.js";/**
  * @license lucide-react v0.394.0 - ISC
  *
  * This source code is licensed under the ISC license.
