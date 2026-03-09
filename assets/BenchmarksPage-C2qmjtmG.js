b141f5072fce0a3b52cdb31a9cb2e457278a9bf1
hholb <hholb@users.noreply.github.com>
2026-03-09 18:11:51 +0000

    fix modal app validation error reporting (#482) Garden-AI/garden-frontend@4f034acdf6759c9c6d2d7cc06b985c9f273f937e

--- assets/BenchmarksPage-C2qmjtmG.js
+++ assets/BenchmarksPage-C2qmjtmG.js
@@ -1,4 +1,4 @@
-import{c as We,j as b,ag as Tf,B as ot,aD as _l,a as mc,b as Wg,e as qg,g as Kg,r as T,an as Ol,ao as Pl,ap as Al,aq as $l,am as Ca,I as Do,x as r0,aE as n0,aF as i0,aG as Lo,aH as a0,aI as Rs,a5 as Ln,aJ as o0,ah as s0,ai as l0,aj as Ds,aK as u0,aL as c0,al as f0,ak as Mf,aM as ua,aN as se,aO as ee,R as P,a1 as Le,u as Fo,aP as Xg,aQ as d0,aR as Yg,D as p0,E as Nf,F as If,H as kf,at as h0,av as g0}from"./index-cAh4-QJL.js";import{S as jl,a as Cl,b as El,c as Tl,d as Ml}from"./select-RRO3B6HK.js";/**
+import{c as We,j as b,ag as Tf,B as ot,aD as _l,a as mc,b as Wg,e as qg,g as Kg,r as T,an as Ol,ao as Pl,ap as Al,aq as $l,am as Ca,I as Do,x as r0,aE as n0,aF as i0,aG as Lo,aH as a0,aI as Rs,a5 as Ln,aJ as o0,ah as s0,ai as l0,aj as Ds,aK as u0,aL as c0,al as f0,ak as Mf,aM as ua,aN as se,aO as ee,R as P,a1 as Le,u as Fo,aP as Xg,aQ as d0,aR as Yg,D as p0,E as Nf,F as If,H as kf,at as h0,av as g0}from"./index-Dcf5Vx5B.js";import{S as jl,a as Cl,b as El,c as Tl,d as Ml}from"./select-kfHCVdr_.js";/**
  * @license lucide-react v0.394.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/CreateGardenPage-Bdi7rm-O.js
+++ assets/CreateGardenPage-Bdi7rm-O.js
@@ -1,4 +1,4 @@
-import{c as v,u as m,j as e,a as d,b as i,d as b,e as x,f as p,g as u,B as h,h as C,i as w,r as F,O as l,k as P}from"./index-cAh4-QJL.js";import{u as S,G as k}from"./GlobusGroupError-BuAMvjye.js";import G from"./ModalAppUploadPage-Uw-GWuOI.js";/**
+import{c as v,u as m,j as e,a as d,b as i,d as b,e as x,f as p,g as u,B as h,h as C,i as w,r as F,O as l,k as P}from"./index-Dcf5Vx5B.js";import{u as S,G as k}from"./GlobusGroupError-DOf2IkR3.js";import G from"./ModalAppUploadPage-Bj3qe0dR.js";/**
  * @license lucide-react v0.394.0 - ISC
  *
  * This source code is licensed under the ISC license.
