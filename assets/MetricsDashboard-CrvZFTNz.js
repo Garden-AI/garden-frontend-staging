33d94fe10ecd82ee7e3ef2219c22a1be2d35d6bf
blaiszik <blaiszik@users.noreply.github.com>
2026-07-06 16:42:15 +0000

    Update home page navigation and footer Garden-AI/garden-frontend@2da5457256778d75fa0df2c4be65e74d3aade844

--- assets/MetricsDashboard-CrvZFTNz.js
+++ assets/MetricsDashboard-CrvZFTNz.js
@@ -1,4 +1,4 @@
-import{c as x,t as u,at as g,j as e,L as m,bd as p,be as b,bf as v,aQ as j,a as f,b as y,e as N,g as G,f as C}from"./index-2UjHSk95.js";/**
+import{c as x,t as u,av as g,j as e,L as m,bf as p,bg as b,bh as v,aS as j,a as f,b as y,e as N,g as G,f as C}from"./index-B4Ig3A50.js";/**
  * @license lucide-react v0.394.0 - ISC
  *
  * This source code is licensed under the ISC license.
